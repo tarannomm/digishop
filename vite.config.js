26910731ee0bbf3b4b8d269a51e8c0487e8892c5
tarannom <89591492+tarannomm@users.noreply.github.com>
2025-03-07 15:20:34 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom"],
+      external: ["@tanstack/react-query", "react-router-dom"],
     },
   },
 });
